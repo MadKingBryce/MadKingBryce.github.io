aac8564ca8d9ed5d0a5e6cc055afd3cff531bce4
Bryce  Greene <bgreene@elevenfiftyconsulting.com>
2016-09-26 08:38:56 -0400

    here

---
+++ w5lab2.py
@@ -0,0 +1,25 @@
+import random
+num1 = input("enter your first numebr:")
+num2 = input ("print")
+num = 0
+list = {1, 2 , 9, 4}
+list.remove(2);
+while num < 10 
+	print(random((0, 100))
+	num = num + 1
+//python sux
+
+
+while true
+	a = input("Put in a num dog ")
+	b = input("Put in a new numbr")
+
+	try:
+		print(int(a) + int(b))
+		break
+	except:
+		print("You fuckded up")
+
+
+
+
